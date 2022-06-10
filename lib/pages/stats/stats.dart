7a14015f4b0d96d2b6d84d61f5b5524c81abcaf8
allhailthethanos <55079966+allhailthethanos@users.noreply.github.com>
2022-06-10 13:33:57 +0530

    added dropdown in stats

--- lib/pages/stats/stats.dart
+++ lib/pages/stats/stats.dart
@@ -1,13 +1,20 @@
 // ignore_for_file: prefer_const_constructors
 // ignore_for_file: prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class Stats extends StatelessWidget {
+class Stats extends StatefulWidget {
   const Stats({Key? key}) : super(key: key);
+
+  @override
+  State<Stats> createState() => _StatsState();
+}
+
+class _StatsState extends State<Stats> {
+  final dropdownItems = ['1 DAY', '1 WEEK ', '1 MONTH'];
+  String? dropdownValue = '1 DAY';
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +22,34 @@ class Stats extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Color(0xFF201c24),
     ));
-
     return Scaffold(
       appBar: AppBar(
         systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarColor: Colors.transparent,
           statusBarIconBrightness: Brightness.light,
         ),
+        actions: [
+          Container(
+            alignment: Alignment.centerRight,
+            width: size.width * 0.3,
+            child: DropdownButton<String>(
+              dropdownColor: Colors.white,
+              underline: DropdownButtonHideUnderline(child: Container()),
+              value: dropdownValue,
+              icon: Icon(
+                Icons.arrow_drop_down_rounded,
+                color: Colors.white,
+              ),
+              isExpanded: true,
+              onChanged: (value) => setState(() => {
+                    //value = this.dropdownValue,
+                    this.dropdownValue = value,
+                  }),
+              items: dropdownItems.map(buildMenuItem).toList(),
+            ),
+          ),
+        ],
+        elevation: 0,
         toolbarHeight: 70, // Set this height
         leading: IconButton(
           icon: SvgPicture.asset(
@@ -32,26 +60,6 @@ class Stats extends StatelessWidget {
           ),
           onPressed: () => Navigator.pop(context),
         ),
-        actions: [
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-            child: Row(
-              children: [
-                Text(
-                  '1 Week',
-                  style: GoogleFonts.montserrat(
-                    textStyle: const TextStyle(
-                      fontSize: 14,
-                      letterSpacing: 2,
-                    ),
-                  ),
-                ),
-                Icon(Icons.arrow_drop_down),
-              ],
-            ),
-            alignment: Alignment.centerRight,
-          ),
-        ],
         backgroundColor: Color(0xFF201c24),
         title: Row(
           children: <Widget>[
@@ -61,7 +69,7 @@ class Stats extends StatelessWidget {
                   'STATISTICS',
                   style: GoogleFonts.montserrat(
                     textStyle: const TextStyle(
-                      fontSize: 22,
+                      fontSize: 20,
                     ),
                   ),
                 ),
@@ -80,15 +88,26 @@ class Stats extends StatelessWidget {
           ],
         ),
       ),
-      body: Container(
-        width: size.width * 0.8,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          // child: TabBar(
-          //   tabs: ['First', 'Second'],
-          // ),
-        ),
-      ),
+      body: Container(),
     );
   }
+
+  DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
+        value: item,
+        child: ListTile(
+          contentPadding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+          minLeadingWidth: 20,
+          title: Text(
+            item,
+            style: GoogleFonts.montserrat(
+              textStyle: const TextStyle(
+                fontSize: 12,
+                letterSpacing: 1.2,
+                color: Color(0xFF808080),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      );
 }
